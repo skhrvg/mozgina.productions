556abdbcd4c24910e9f76387e6ae786f9702052a
Gleb Sakharov <35118149+skhrvg@users.noreply.github.com>
2023-10-13 23:42:09 +0300

    fix custom cursor visibility on touch devices

--- src/components/TheCursor.vue
+++ src/components/TheCursor.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { useMouse, usePageLeave } from '@vueuse/core'
+import { useMouse, usePageLeave, usePointer } from '@vueuse/core'
 import { ref } from 'vue'
 
 const { x, y } = useMouse({
   type: 'client',
   touch: false,
   initialValue: { x: -1000, y: -1000 },
-
 })
+const { pointerType } = usePointer()
 const isLeft = usePageLeave()
 const isClickable = ref<boolean>(false)
 
@@ -23,7 +23,7 @@ clickables.forEach(el => el.addEventListener('mouseleave', () => {
 <template>
   <div
     class="fixed w-8 h-8 border-2 border-black rounded-full duration-100 pointer-events-none opacity-50 -translate-x-1/2 -translate-y-1/2"
-    :class="{ '!opacity-0': isLeft || isClickable }"
+    :class="{ '!opacity-0': isLeft || isClickable || pointerType !== 'mouse' }"
     :style="{
       left: `${x}px`,
       top: `${y}px`,
@@ -32,7 +32,7 @@ clickables.forEach(el => el.addEventListener('mouseleave', () => {
   <div
     class="fixed w-1.5 h-1.5 bg-black rounded-full duration-75 pointer-events-none opacity-50 -translate-x-1/2 -translate-y-1/2"
     :class="{
-      '!opacity-0': isLeft,
+      '!opacity-0': isLeft || pointerType !== 'mouse',
       'w-16 h-16': isClickable,
     }"
     :style="{
